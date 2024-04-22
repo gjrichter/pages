6222c8f51d28e2d6b13609847243fa1173e52fdd
gjrichter <guenter.richter@gmail.com>
2024-04-22 14:05:35 +0200

    new (2.) PUN map

--- PUN/show_facets.js
+++ PUN/show_facets.js
@@ -558,6 +558,10 @@ ixmaps.data = ixmaps.data || {};
 				}
 			} else
 			if (facetsA[i].values) {
+				
+				if (facetsA[i].values.length == 0){
+					szHtml += '<div>'+facetsA[i].values.length+'</div>';
+				}
 
 				// ---------------------------------
 				// type B
@@ -623,8 +627,6 @@ ixmaps.data = ixmaps.data || {};
 						}
 
 						var nWidth = (100 / nMaxCount * nCount);
-						console.log(facetsA[i].id);
-						console.log(nWidth);
 						if (!isNaN(nWidth) && (nWidth < 100) && (facetsA[i].uniqueValues >= 2)) {
 							
 							szHtml += '<div style="position:relative;top:3px;left:0.1em;background:'+bgColor+';line-height:0.4em;width:' + nWidth + '%;border-radius:0 0.5em 0.5em 0">&nbsp;</div>';
